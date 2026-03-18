9b29c51602d3abe34be925a521712cd88fb2cbbf
fadhelcherif <99729368+fadhelcherif@users.noreply.github.com>
2026-03-18 23:01:25 +0100

    Initial commit

---
+++ C#_GestionDepenses/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C__GestionDepenses.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
